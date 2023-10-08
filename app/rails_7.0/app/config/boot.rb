cca31f5abc7a93a5655a6617856c01fd29963e21
Marian13 <mariankostyk13895@gmail.com>
2023-10-08 19:03:26 +0300

    refactor(rails_common): extract database.yml and database prefix

--- app/rails_7.0/app/config/boot.rb
+++ app/rails_7.0/app/config/boot.rb
@@ -3,4 +3,4 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
 
-require_relative "../../../database_prefix_env_variable"
+require_relative "../../../rails_common/database_prefix_env_variable"
